8385b9851084a1ba22c7b8b49aaec8390925e2b2
Marione-Pereira <mari.tainara@gmail.com>
2022-05-08 12:58:55 -0300

    ajuste conflitos testes
    Co-authored-by: KarinaMel0 <karinamelarantessilva@gmail.com>
    Co-authored-by: natalieiss <natalie_ingrid@hotmail.com>
    Co-authored-by: Marione-Tainara <mari.tainara@gmail.com>

--- src/login/login.js
+++ src/login/login.js
@@ -13,8 +13,6 @@ export const login = () => {
   <input type="email" name="email" class="email-input input-users" placeholder="Preencha com o e-mail" autocomplete required /><br>
   <input type="password" name="password" class="password-input input-users" placeholder="Preencha com a senha" required /><br>
   <button class='btn-submit' type="submit">Entrar</button><br>
-  <a href="" > <p class='reset-password'>Esqueceu a sua senha? Clique aqui</p></a><br>
-  <br>
   <button class="btn-google"><img src="img/google.png" alt="botão Google">Efetuar login com o Google
   </button><br>
 </form>
@@ -60,7 +58,7 @@ export const login = () => {
       googleLogin().then(() => {
         window.location.hash = '#timeline';
       });
-    }),
+    })
   );
   return containerLogin;
 };
